926331ecf59419e474f09394a0075e79c810eadd
Sakshi Antala <sakshi@saasinnova.com>
2024-11-08 09:38:56 +0530

    replace capitalize class with capitalize-first and modify the key

--- DisplayInfo.tsx
+++ DisplayInfo.tsx
@@ -6,13 +6,13 @@ export const DisplayInfo = (props: IDisplayInfo) => {
   return (
     <table className="line-height-2 surface-100 w-full display-info-table">
       <tbody>
-        {Object.entries(infoData).map(([key, value]: any, index) => {
+        {Object.entries(infoData).map(([key, value]: any) => {
           return (
-            <tr key={index}>
-              <td className="p-1 font-semibold capitalize text-900 w-6">
+            <tr key={key}>
+              <td className="p-1 font-semibold capitalize-first text-900 w-6">
                 {key}:
               </td>
-              <td className="p-1 capitalize w-6"> {value}</td>
+              <td className="p-1 capitalize-first w-6"> {value}</td>
             </tr>
           );
         })}
